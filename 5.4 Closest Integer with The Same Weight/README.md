a110608c622916953d94445034e3200ad8bf317d
Altunshukurlu <shuka01@students.ipfw.edu>
2016-01-12 18:58:09 -0500

    Finished pr. 5.4

--- 5.4 Closest Integer with The Same Weight/README.md
+++ 5.4 Closest Integer with The Same Weight/README.md
@@ -1,10 +1,11 @@
 <h1>5.4 Closest integer with the same weight</h1>
 
-**INFORMATION**
+**COMMENTS**
 
+Fairly simple problem. Closest y to x will be the same binary with the first different bits in x switched. If weight is 0 or 64, x will not have any different consecutive bits, therefore weight must be between 0 and 64. Once the place of first consecutive bits is found, swapping them is enough to find y. Refer to 5.2 for swapping. 
 
 **BOOK SOLUTIONS**
 
-1. Manipulation using bit operations
+1. Same as stated above
 
 pg 174 for more information
--- 5.4 Closest Integer with The Same Weight/main.c
+++ 5.4 Closest Integer with The Same Weight/main.c
@@ -3,14 +3,24 @@
 #include <stdio.h>
 
 long find_closest(long x){
-	x = x ^ (!0); 
-	x = x & !(x-1) ;
-	return x; 
+	int i = 0; 
+	for(i=0; i<63; i++){
+		int first_bit = (x >> i) & 1 ;
+		int second_bit = (x >> (i+1)) & 1; 
+		printf("%d %d\n", first_bit, second_bit); 
+		if(first_bit != second_bit){
+			//swap those consecutive bits, that's our y. 
+			x = x ^ ( 1<<i | 1<<(i+1) ); 
+			return x; 
+		}
+	}
+	return -1; //weight is not in the range. 
 }
 
 int main()
 {
-
-    return 0; 
+	long x = 1000;
+	printf("Closest integer to %ld is %ld \n", x, find_closest(x)); 
+	return 0; 
 
 }
